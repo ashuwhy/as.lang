b07b03f4aef78d59641fd8997f0a12552f7430ee
ashuwhy <ashutoshsharmawhy@gmail.com>
2025-01-05 22:12:03 +0530

    tinking of getting on it

---
+++ main.as
@@ -0,0 +1,6 @@
+name = input "Enter your name: "
+if name == "Ed Sheeran" {
+    output "Hi, " + name
+} else {
+    output "Bye, " + name
+}
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from sys import argv
 from setuptools import setup, find_packages
 
 
-if argv[1] in ('install', 'build', 'sdist', 'bdist_wheel'):
+if len(argv) > 1 and argv[1] in ('install', 'build', 'sdist', 'bdist_wheel'):
   pass #will add file assoc soon  
 
  
